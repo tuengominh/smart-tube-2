3d8f0256ef8b2d7806f653420f2b87a0dedb5b68
tuengominh <tue.ngominh@gmail.com>
2019-02-22 17:54:36 +0100

    create project

---
+++ src/Counter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class Counter extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            hasData: false,
+            data: [],
+        };
+        //this.fetchData = this.fetchData.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get('http://192.168.0.110:5000/tubes/12345')
+            .then(results => results.json())
+            .then((results) => {
+                this.setState({
+                    hasData: true,
+                    data: results,
+                }, () => {
+                });
+            }).catch(err => {
+                console.log(err);
+                this.setState({hasData: false,})
+        })
+    }
+
+    render() {
+            let counter = this.state.data.length;
+            return("You have " + this.state.hasData + (5-counter) + (counter === 1 ? " time left" : " times left"));
+    }
+
+    //fetchData(url) {
+    //}
+}
+
+export default Counter;
